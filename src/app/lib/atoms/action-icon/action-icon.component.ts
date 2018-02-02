af4442dbc6567c1ec55ee4982d7dc34693b22490
佐藤 有斗 <yuito.sato@bizreach.co.jp>
2018-02-02 19:07:31 +0900

    : )

--- src/app/lib/atoms/action-icon/action-icon.component.ts
+++ src/app/lib/atoms/action-icon/action-icon.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+const iconClass = {
+  add:
+}
+
 @Component({
   selector: 'adp-action-icon',
   templateUrl: './action-icon.component.html',
